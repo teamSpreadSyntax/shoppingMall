99042de27b613c14347ca2baf3aa769529025002
kimpokpung <gh8897@naver.com>
2024-02-08 19:39:32 +0900

    2402081939

--- src/main/java/home/project/domain/Member.java
+++ src/main/java/home/project/domain/Member.java
@@ -1,16 +1,16 @@
 package home.project.domain;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "member")
+@Getter
+@Setter
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    public void setId(Long id) { this.id = id;    }
-
-    public Long getId() { return id;    }
 
 }
