83100d3b52cb29a2ebf1be21384aa6a596c39d4a
kimpokpung <gh8897@naver.com>
2024-02-09 22:51:00 +0900

    2402091050규현

--- src/main/java/home/project/domain/Member.java
+++ src/main/java/home/project/domain/Member.java
@@ -12,41 +12,41 @@ import java.sql.Date;
 @Setter
 public class Member {
     @Id
-    private Long idd;
+    private Long id;
 
     @Column(name = "name")
     private String name;
-//
-//    @Column(name = "sex")
-//    private String sex;
-//
-//    @Column(name = "birth")
-//    private Date birth;
-//
-//    @Column(name = "age")
-//    private Integer age;
-//
-//    @Column(name = "address_num")
-//    private String address_num;
-//
-//    @Column(name = "address")
-//    private String address;
-//
-//    @Column(name = "address_detail")
-//    private String address_detail;
-//
-//    @Column(name = "address_name")
-//    private String address_name;
-//
-//    @Column(name = "phone")
-//    private String phone;
-//
-//    @Column(name = "mileage")
-//    private Integer mileage;
-//
-//    @Column(name = "coupon")
-//    private String coupon;
-//
-//    @Column(name = "total")
-//    private String total;
+
+    @Column(name = "sex")
+    private String sex;
+
+    @Column(name = "birth")
+    private Date birth;
+
+    @Column(name = "age")
+    private Integer age;
+
+    @Column(name = "address_num")
+    private String address_num;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "address_detail")
+    private String address_detail;
+
+    @Column(name = "address_name")
+    private String address_name;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "mileage")
+    private Integer mileage;
+
+    @Column(name = "coupon")
+    private String coupon;
+
+    @Column(name = "total")
+    private String total;
 }
