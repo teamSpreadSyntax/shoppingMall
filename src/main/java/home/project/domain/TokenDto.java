bff823f1b2da263ffbf08604f0dce86b5f378f0e
kimpokpung <gh8897@naver.com>
2024-03-17 22:32:26 +0900

    2403172232 규현

--- src/main/java/home/project/domain/TokenDto.java
+++ src/main/java/home/project/domain/TokenDto.java
@@ -1,20 +1,20 @@
-//package home.project.domain;
-//
-//import lombok.*;
-//
-//@Getter
-//@Setter
-//@Builder
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class TokenDto {
-//    private String grantType;
-//    private String accessToken;
-//    private String refreshToken;
-//    private Long accessTokenExpireln;
-//    private Long refreshTokenExpiresin;
-//    private String authority;
-//    private String info;
-//    public void setInfo(String info) { this.info = info;}
-//}
-//
+package home.project.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+    private String grantType;
+    private String accessToken;
+    private String refreshToken;
+    private Long accessTokenExpireln;
+    private Long refreshTokenExpiresin;
+    private String authority;
+    private String info;
+    public void setInfo(String info) { this.info = info;}
+}
+
