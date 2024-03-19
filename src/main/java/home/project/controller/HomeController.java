f24b4c3286e8296f1060a4e90dd896dfa5b633c6
sksdm <sksdmltkek14@gmail.com>
2024-03-20 00:40:10 +0900

    민서 202403200002

--- src/main/java/home/project/controller/HomeController.java
+++ src/main/java/home/project/controller/HomeController.java
@@ -3,10 +3,10 @@ package home.project.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
-
-@Controller
+@RestController
 public class HomeController {
 
     @GetMapping("/")
