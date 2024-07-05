b8e2a558cb4ea74e05da3828459098e7e45147e7
kimpokpung <gh8897@naver.com>
2024-07-05 19:11:10 +0900

    2407051911 규현

--- src/test/java/home/project/service/MemberServiceImplTest.java
+++ src/test/java/home/project/service/MemberServiceImplTest.java
@@ -1,6 +1,7 @@
 package home.project.service;
 
 import home.project.domain.Member;
+import home.project.domain.Product;
 import home.project.repository.MemberRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -46,29 +47,29 @@ class MemberServiceImplTest {
         member = new Member();
         member.setId(1L);
         member.setEmail("test@example.com");
-        member.setPhone("010-1234-5678");
+        member.setPhone("010-1111-1111");
         member.setPassword("password");
-        member.setName("강민서");
+        member.setName("김길동");
 
         member2 = new Member();
         member2.setId(2L);
         member2.setEmail("test2@example.com");
-        member2.setPhone("010-8765-4321");
-        member2.setPassword("anotherPassword");
-        member2.setName("강민석");
+        member2.setPhone("010-2222-2222");
+        member2.setPassword("otherPassword");
+        member2.setName("홍길동");
 
         member3 = new Member();
         member3.setId(3L);
         member3.setEmail("test3@example.com");
-        member3.setPhone("010-1234-4567");
-        member3.setPassword("anotherPassword2");
-        member3.setName("김규현");
+        member3.setPhone("010-3333-3333");
+        member3.setPassword("anotherPassword");
+        member3.setName("박길동");
     }
 
     @Nested
     class JoinTests {
         @Test
-        void shouldThrowExceptionWhenEmailAndPhoneDuplicate() {
+        void shouldThrowExceptionWhenJoinWithDuplicateEmailAndPhone() {
             when(memberRepository.existsByEmail(member.getEmail())).thenReturn(true);
             when(memberRepository.existsByPhone(member.getPhone())).thenReturn(true);
 
@@ -77,7 +78,7 @@ class MemberServiceImplTest {
         }
 
         @Test
-        void shouldThrowExceptionWhenEmailDuplicate() {
+        void shouldThrowExceptionWhenJoinWithDuplicateEmail() {
             when(memberRepository.existsByEmail(member.getEmail())).thenReturn(true);
             when(memberRepository.existsByPhone(member.getPhone())).thenReturn(false);
 
@@ -86,7 +87,7 @@ class MemberServiceImplTest {
         }
 
         @Test
-        void shouldThrowExceptionWhenPhoneDuplicate() {
+        void shouldThrowExceptionWhenJoinWithDuplicatePhone() {
             when(memberRepository.existsByEmail(member.getEmail())).thenReturn(false);
             when(memberRepository.existsByPhone(member.getPhone())).thenReturn(true);
 
@@ -95,7 +96,7 @@ class MemberServiceImplTest {
         }
 
         @Test
-        void shouldRegisterMemberSuccessfully() {
+        void shouldJoinMemberSuccessfully() {
             when(memberRepository.existsByEmail(member.getEmail())).thenReturn(false);
             when(memberRepository.existsByPhone(member.getPhone())).thenReturn(false);
             when(passwordEncoder.encode(member.getPassword())).thenReturn("encodedPassword");
@@ -128,10 +129,11 @@ class MemberServiceImplTest {
             assertEquals("1로 등록된 회원이 없습니다.", exception.getMessage());
         }
     }
+
     @Nested
-    class FindMembersTests {
+    class FindAllTests {
         @Test
-        void shouldFindAllMembers() {
+        void shouldFindAllMembersSuccessfully() {
             Pageable pageable = PageRequest.of(0, 10);
             Page<Member> page = new PageImpl<>(Arrays.asList(member, member2));
 
@@ -145,46 +147,60 @@ class MemberServiceImplTest {
             Member secondMember = resultList.getContent().get(1);
             assertEquals(member2, secondMember);
         }
+    }
 
+    @Nested
+    class FindMembersTests {
         @Test
-        void shouldFindMembersByNameSuccessfully() {
+        void shouldFindMembersByConditionSuccessfully() {
             Pageable pageable = PageRequest.of(0, 10);
             Page<Member> page = new PageImpl<>(Arrays.asList(member, member2));
 
-            when(memberRepository.findMembers("강민서", "null", "null", "null", pageable)).thenReturn(page);
+            when(memberRepository.findMembers("김길동", "null", "null", "null", pageable)).thenReturn(page);
 
-            Page<Member> resultList = memberService.findMembers("강민서", "null", "null", "null", pageable);
+            Page<Member> resultList = memberService.findMembers("김길동", "null", "null", "null", pageable);
 
             assertNotNull(resultList);
             assertEquals(2, resultList.getTotalElements());
             Member firstMember = resultList.getContent().get(0);
-            assertEquals("강민서", firstMember.getName());
+            assertEquals("김길동", firstMember.getName());
             Member secondMember = resultList.getContent().get(1);
-            assertEquals("강민석", secondMember.getName());
+            assertEquals("홍길동", secondMember.getName());
         }
+
+        @Test
+        void shouldThrowsExceptionWhenCanNotFoundMember() {
+            Pageable pageable = PageRequest.of(0, 10);
+            Page<Member> emptyPage = Page.empty(pageable);
+
+            when(memberRepository.findMembers("1", "2", "3", null, pageable)).thenReturn(emptyPage);
+
+            IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> memberService.findMembers("1", "2", "3", null, pageable));
+            assertEquals("해당하는 회원이 없습니다.", exception.getMessage());
+        }
+
     }
+
     @Nested
     class UpdateTests {
         @Test
         void shouldUpdateMemberSuccessfully() {
             when(memberRepository.findById(1L)).thenReturn(Optional.of(member));
-            when(memberRepository.existsByEmail("sksdmltkek12@naver.com")).thenReturn(false);
-            when(memberRepository.existsByPhone("010-1234-5672")).thenReturn(false);
             when(passwordEncoder.encode("newPassword")).thenReturn("encodeNewPassword");
 
             Member updateMember = new Member();
             updateMember.setId(member.getId());
-            updateMember.setEmail("sksdmltkek12@naver.com");
-            updateMember.setPhone("010-1234-5672");
+            updateMember.setEmail("test@example.com");
+            updateMember.setPhone("010-1234-5678");
             updateMember.setPassword("newPassword");
-            updateMember.setName("강민수");
+            updateMember.setName("김길동");
 
             Optional<Member> resultMember = memberService.update(updateMember);
             assertTrue(resultMember.isPresent());
-            assertEquals("sksdmltkek12@naver.com", resultMember.get().getEmail());
-            assertEquals("010-1234-5672", resultMember.get().getPhone());
+            assertEquals("test@example.com", resultMember.get().getEmail());
+            assertEquals("010-1234-5678", resultMember.get().getPhone());
             assertEquals("encodeNewPassword", resultMember.get().getPassword());
-            assertEquals("강민수", resultMember.get().getName());
+            assertEquals("김길동", resultMember.get().getName());
         }
     }
 
---
+++ src/test/java/home/project/service/RoleServiceImplTest.java
@@ -0,0 +1,88 @@
+package home.project.service;
+
+import home.project.domain.Role;
+import home.project.repository.RoleRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class RoleServiceImplTest {
+
+    @Mock
+    private RoleRepository roleRepository;
+
+    @InjectMocks
+    private RoleServiceImpl roleService;
+
+    private Role role;
+
+    @BeforeEach
+    void setUp(){
+        role = new Role();
+        role.setId(1L);
+        role.setRole("user");
+    }
+
+    @Nested
+    class FindByIdTests {
+        @Test
+        void shouldFindRoleByIdSuccessfully() {
+            when(roleRepository.findById(1L)).thenReturn(Optional.of(role));
+            Optional<Role> findRole = roleService.findById(1L);
+
+            assertTrue(findRole.isPresent());
+            assertEquals(role, findRole.get());
+        }
+
+        @Test
+        void shouldThrowExceptionWhenRoleNotFoundById() {
+            when(roleRepository.findById(1L)).thenReturn(Optional.empty());
+
+            IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> roleService.findById(1L));
+            assertEquals("1로 등록된 회원이 없습니다.", exception.getMessage());
+        }
+    }
+
+    @Nested
+    class UpdateTests {
+        @Test
+        void shouldUpdateRoleSuccessfully() {
+            when(roleRepository.findById(1L)).thenReturn(Optional.of(role));
+
+            Role updateRole = new Role();
+            updateRole.setId(role.getId());
+            updateRole.setRole("user");
+
+            Optional<Role> resultRole = roleService.update(updateRole);
+            assertTrue(resultRole.isPresent());
+            assertEquals("user", resultRole.get().getRole());
+        }
+    }
+
+    @Nested
+    class JoinTests {
+        @Test
+        void shouldJoinRoleSuccessfully() {
+            when(roleRepository.save(role)).thenReturn(role);
+
+            roleService.join(role);
+
+            verify(roleRepository).save(role);
+
+            assertEquals("user", role.getRole());
+            assertEquals(1L, role.getId());
+        }
+    }
+
+}
