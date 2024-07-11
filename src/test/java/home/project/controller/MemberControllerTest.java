3f4463fb5658a47dea281674db9d2c66788c817e
kms199406 <sksdmltkek14@gmail.com>
2024-07-11 15:13:37 +0900

    202407052125 강민서
    시도1

--- src/test/java/home/project/controller/MemberControllerTest.java
+++ src/test/java/home/project/controller/MemberControllerTest.java
@@ -1,153 +1,98 @@
 package home.project.controller;
 
-import home.project.domain.*;
+import home.project.SecurityConfig;
+import home.project.domain.Member;
+import home.project.domain.MemberDTOWithoutId;
+import home.project.domain.Role;
+import home.project.domain.TokenDto;
 import home.project.service.JwtTokenProvider;
 import home.project.service.MemberService;
 import home.project.service.RoleService;
 import home.project.service.ValidationCheck;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.validation.BindingResult;
 
-import java.util.*;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@WebMvcTest(controllers = MemberController.class)
+@Import({SecurityConfig.class})
 public class MemberControllerTest {
 
-    @Mock
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
     private MemberService memberService;
 
-    @Mock
-    private JwtTokenProvider jwtTokenProvider;
-
-    @Mock
-    private ValidationCheck validationCheck;
-
-    @Mock
+    @MockBean
     private RoleService roleService;
 
+    @MockBean
+    private JwtTokenProvider jwtTokenProvider;
 
-    @InjectMocks
-    private MemberController memberController;
+    @MockBean
+    private ValidationCheck validationCheck;
+
+    @MockBean
+    private UserDetailsService userDetailsService;
+
+    @MockBean
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private BindingResult bindingResult;
 
     @BeforeEach
     public void setUp() {
-        MockitoAnnotations.initMocks(this);
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
-    public void testCreateMember_Success() {
+    public void testCreateMember_Success() throws Exception {
         MemberDTOWithoutId memberDTO = new MemberDTOWithoutId();
         memberDTO.setEmail("test@example.com");
         memberDTO.setPassword("123456");
         memberDTO.setName("강민서");
         memberDTO.setPhone("010-1234-5678");
-        BindingResult bindingResult = mock(BindingResult.class);
+
+        when(validationCheck.validationChecks(bindingResult)).thenReturn(null);
+
         Member member = new Member();
         member.setId(1L);
         member.setEmail(memberDTO.getEmail());
         member.setPassword(memberDTO.getPassword());
         member.setName(memberDTO.getName());
         member.setPhone(memberDTO.getPhone());
+
         Role role = new Role();
         role.setId(1L);
         role.setRole("user");
 
+        when(memberService.findByEmail(memberDTO.getEmail())).thenReturn(Optional.empty());
+        when(roleService.findById(1L)).thenReturn(Optional.of(role));
+        doNothing().when(memberService).join(any(Member.class));
+        when(jwtTokenProvider.generateToken(any())).thenReturn(new TokenDto("bearer", "accessToken", "refreshToken"));
 
-        when(validationCheck.validationChecks(bindingResult)).thenReturn(null);
-        when(memberService.findByEmail(memberDTO.getEmail())).thenReturn(Optional.of(member));
-        when(roleService.findById(member.getId())).thenReturn(Optional.of(role));
-        when(jwtTokenProvider.generateToken(any())).thenReturn(new TokenDto("bearer","accessToken", "refreshToken"));
-
-        CustomOptionalResponseEntity<?> responseEntity = memberController.createMember(memberDTO, bindingResult);
-
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-/*
-        assertTrue(responseEntity.getBody().result.isPresent());
-*/
+        mockMvc.perform(post("/api/member/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{ \"email\": \"test@example.com\", \"password\": \"123456\", \"name\": \"강민서\", \"phone\": \"010-1234-5678\" }"))
+                .andExpect(status().isOk());
     }
-
-   /* @Test
-    public void testFindMemberById_Success() {
-        Long memberId = 1L;
-        Member member = new Member();
-        member.setId(memberId);
-        when(memberService.findById(memberId)).thenReturn(Optional.of(member));
-
-        CustomOptionalResponseEntity<Optional<Member>> responseEntity = memberController.findMemberById(memberId);
-
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertTrue(responseEntity.getBody().getResult().isPresent());
-        assertEquals(memberId, ((Member) responseEntity.getBody().getResult().get()).getId());
-    }
-
-    @Test
-    public void testFindAllMember_Success() {
-        Pageable pageable = PageRequest.of(0, 5);
-        Member member = new Member();
-        member.setId(1L);
-        Page<Member> memberPage = new PageImpl<>(Collections.singletonList(member));
-
-        when(memberService.findAll(pageable)).thenReturn(memberPage);
-
-        ResponseEntity<?> responseEntity = memberController.findAllMember(pageable);
-
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-    }
-
-    @Test
-    public void testSearchMembers_Success() {
-        Pageable pageable = PageRequest.of(0, 5);
-        Member member = new Member();
-        member.setId(1L);
-        Page<Member> memberPage = new PageImpl<>(Collections.singletonList(member));
-
-        when(memberService.findMembers(null, null, null, null, pageable)).thenReturn(memberPage);
-
-        ResponseEntity<?> responseEntity = memberController.searchMembers(null, null, null, null, pageable);
-
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-    }
-
-    @Test
-    public void testUpdateMember_Success() {
-        Member member = new Member();
-        member.setId(1L);
-        member.setEmail("updated@example.com");
-        member.setPassword("updatedPassword");
-        member.setName("Updated User");
-        member.setPhone("010-9876-5432");
-        BindingResult bindingResult = mock(BindingResult.class);
-
-        when(validationCheck.validationChecks(bindingResult)).thenReturn(null);
-        when(memberService.update(member)).thenReturn(Optional.of(member));
-
-        CustomOptionalResponseEntity<?> responseEntity = memberController.updateMember(member, bindingResult);
-
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertTrue(responseEntity.getBody().getResult().isPresent());
-        assertEquals(member.getEmail(), ((Member) responseEntity.getBody().getResult().get()).getEmail());
-    }
-
-    @Test
-    public void testDeleteMember_Success() {
-        Long memberId = 1L;
-
-        CustomOptionalResponseEntity<Optional<Member>> responseEntity = memberController.deleteMember(memberId);
-
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-    }*/
 }
--- src/test/java/home/project/controller/ProductControllerTest.java
+++ src/test/java/home/project/controller/ProductControllerTest.java
@@ -1,183 +1,103 @@
 package home.project.controller;
 
-import home.project.controller.ProductController;
-import home.project.domain.*;
+import home.project.SecurityConfig;
+import home.project.domain.Product;
+import home.project.domain.ProductDTOWithoutId;
+import home.project.service.JwtTokenProvider;
 import home.project.service.ProductService;
 import home.project.service.ValidationCheck;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.validation.BindingResult;
 
-import java.util.*;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@WebMvcTest(controllers = ProductController.class, excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = SecurityConfig.class)
+})
+@Import({SecurityConfig.class, ProductControllerTest.TestConfig.class})
+@EnableWebSecurity
 public class ProductControllerTest {
 
-    private ProductController productController;
+    @Configuration
+    static class TestConfig {
+        @MockBean
+        public UserDetailsService userDetailsService;
 
-    @Mock
+        @MockBean
+        public PasswordEncoder passwordEncoder;
+
+        @MockBean
+        public JwtTokenProvider jwtTokenProvider;
+
+        @Bean
+        public SecurityConfig securityConfig(JwtTokenProvider jwtTokenProvider) {
+            return new SecurityConfig(jwtTokenProvider);
+        }
+    }
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
     private ProductService productService;
 
-    @Mock
+    @MockBean
     private ValidationCheck validationCheck;
 
     @Mock
     private BindingResult bindingResult;
 
     @BeforeEach
-    public void setup() {
+    public void setUp() {
         MockitoAnnotations.openMocks(this);
-        productController = new ProductController(productService, validationCheck);
     }
 
     @Test
-    public void testCreateProduct_Success() {
-        ProductDTOWithoutId productDTOWithoutId = new ProductDTOWithoutId();
-        productDTOWithoutId.setBrand("Brand");
-        productDTOWithoutId.setCategory("Category");
-        productDTOWithoutId.setSelledcount(10L);
-        productDTOWithoutId.setName("Product");
-        productDTOWithoutId.setStock(100L);
-        productDTOWithoutId.setImage("image.jpg");
+    public void testCreateProduct_Success() throws Exception {
+        ProductDTOWithoutId productDTO = new ProductDTOWithoutId();
+        productDTO.setBrand("Test Brand");
+        productDTO.setCategory("Test Category");
+        productDTO.setSelledcount(10L);
+        productDTO.setName("Test Product");
+        productDTO.setStock(100L);
+        productDTO.setImage("test_image.jpg");
 
         when(validationCheck.validationChecks(bindingResult)).thenReturn(null);
 
         Product product = new Product();
         product.setId(1L);
-        product.setBrand(productDTOWithoutId.getBrand());
-        product.setCategory(productDTOWithoutId.getCategory());
-        product.setSelledcount(productDTOWithoutId.getSelledcount());
-        product.setName(productDTOWithoutId.getName());
-        product.setStock(productDTOWithoutId.getStock());
-        product.setImage(productDTOWithoutId.getImage());
+        product.setBrand(productDTO.getBrand());
+        product.setCategory(productDTO.getCategory());
+        product.setSelledcount(productDTO.getSelledcount());
+        product.setName(productDTO.getName());
+        product.setStock(productDTO.getStock());
+        product.setImage(productDTO.getImage());
 
         doNothing().when(productService).join(any(Product.class));
 
-        CustomOptionalResponseEntity<?> responseEntity = productController.createProduct(productDTOWithoutId, bindingResult);
-
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-    }
-
-    @Test
-    public void testFindProductById_Success() {
-        Product product = new Product();
-        product.setId(1L);
-        product.setName("Product");
-
-        when(productService.findById(1L)).thenReturn(Optional.of(product));
-
-        CustomOptionalResponseEntity<Optional<Product>> responseEntity = productController.findProductById(1L);
-
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals("1에 해당하는 상품 입니다", responseEntity.getBody().ResponseMessage);
-        assertEquals(Optional.of(product), responseEntity.getBody().result.get());
-    }
-
-    @Test
-    public void testFindAllProduct_Success() {
-        Product product = new Product();
-        product.setId(1L);
-        product.setName("Product");
-
-        List<Product> products = Collections.singletonList(product);
-        Page<Product> productPage = new PageImpl<>(products);
-
-        when(productService.findAll(any(Pageable.class))).thenReturn(productPage);
-
-        CustomListResponseEntity<Product> responseEntity = productController.findAllProduct(PageRequest.of(1, 5));
-
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals("전체 상품입니다", responseEntity.getBody().responseMessage);
-        assertEquals(1 , productPage.getTotalElements());
-    }
-
-    @Test
-    public void testSearchProducts_Success() {
-        Product product = new Product();
-        product.setId(1L);
-        product.setName("Product");
-
-        List<Product> products = Collections.singletonList(product);
-        Page<Product> productPage = new PageImpl<>(products);
-
-        when(productService.findProducts(anyString(), anyString(), anyString(), anyString(), any(Pageable.class))).thenReturn(productPage);
-
-        ResponseEntity<?> responseEntity = productController.searchProducts("Brand", "Category", "Product", "Content", PageRequest.of(0, 5));
-
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-    }
-
-    @Test
-    public void testDeleteProduct_Success() {
-        doNothing().when(productService).deleteById(1L);
-
-        CustomOptionalResponseEntity<Optional<Product>> responseEntity = productController.deleteProduct(1L);
-
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-    }
-
-    @Test
-    public void testIncreaseStock_Success() {
-        Product product = new Product();
-        product.setId(1L);
-        product.setName("Product");
-        product.setStock(150L);
-
-        when(productService.increaseStock(1L, 50L)).thenReturn(product);
-
-        CustomOptionalResponseEntity<Product> responseEntity = productController.increaseStock(1L, 50L);
-
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals("Product 상품이50개 증가하여150개가 되었습니다", responseEntity.getBody().ResponseMessage);
-        assertEquals(product, responseEntity.getBody().result.get());
-    }
-
-    @Test
-    public void testDecreaseStock_Success() {
-        Product product = new Product();
-        product.setId(1L);
-        product.setName("Product");
-        product.setStock(50L);
-
-        when(productService.decreaseStock(1L, 50L)).thenReturn();
-
-        CustomOptionalResponseEntity<Product> responseEntity = productController.decreaseStock(1L, 50L);
-
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals("Product상품이50개 감소하여0개가 되었습니다", responseEntity.getBody().ResponseMessage);
-        assertEquals(product, responseEntity.getBody().result.get());
-    }
-
-    @Test
-    public void testUpdateProduct_Success() {
-        Product product = new Product();
-        product.setId(1L);
-        product.setBrand("UpdatedBrand");
-        product.setCategory("UpdatedCategory");
-        product.setName("UpdatedProduct");
-        product.setStock(100L);
-        product.setImage("updated_image.jpg");
-
-        when(validationCheck.validationChecks(bindingResult)).thenReturn(null);
-        when(productService.update(any(Product.class))).thenReturn(Optional.of(product));
-
-        CustomOptionalResponseEntity<?> responseEntity = productController.updateProduct(product, bindingResult);
-
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals("상품정보가 수정되었습니다", responseEntity.getBody().ResponseMessage);
-        assertEquals(product, responseEntity.getBody().result.get());
+        mockMvc.perform(post("/api/product/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{ \"brand\": \"Test Brand\", \"category\": \"Test Category\", \"selledcount\": 10, \"name\": \"Test Product\", \"stock\": 100, \"image\": \"test_image.jpg\" }"))
+                .andExpect(status().isOk());
     }
 }
