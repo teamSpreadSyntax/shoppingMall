a8b18d8aa85420ebde1e40a2292f03eaa9f054fb
kimpokpung <gh8897@naver.com>
2024-08-20 01:32:59 +0900

    2408200132 규현

--- src/main/java/home/project/controller/MemberController.java
+++ src/main/java/home/project/controller/MemberController.java
@@ -90,11 +90,12 @@ public class MemberController {
         Long id = memberForAddRole.get().getId();
         role.setId(id);
         roleService.join(role);
+        String savedRole=roleService.findById(id).get().getRole();
         TokenDto tokenDto = jwtTokenProvider.generateToken(new UsernamePasswordAuthenticationToken(member.getEmail(), member.getPassword()));
         Map<String, String> responseMap = new HashMap<>();
         responseMap.put("accessToken", tokenDto.getAccessToken());
         responseMap.put("refreshToken", tokenDto.getRefreshToken());
-        responseMap.put("role", id.toString());
+        responseMap.put("role", savedRole);
         responseMap.put("successMessage", "회원가입이 성공적으로 완료되었습니다.");
         return new CustomOptionalResponseEntity<>(Optional.of(responseMap), "회원가입 성공", HttpStatus.OK);
     }
