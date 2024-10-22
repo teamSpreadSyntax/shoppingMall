413ace6a650dcab7124a02e64ae6abc6309fc82d
kimpokpung <gh8897@naver.com>
2024-10-23 03:58:43 +0900

    <작업일시> 24-10-23 03:58
    <작업자>
    김규현
    
    <작업내용>
    1. 도커 설정중

---
+++ src/main/java/home/project/config/KafkaTopicConfig.java
@@ -0,0 +1,30 @@
+package home.project.config;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic createTopic() {
+        return TopicBuilder.name("my-topic")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+}
