6f99d7a9c41878f60f19e8f5f995f339ada6424f
kimpokpung <gh8897@naver.com>
2024-10-24 16:25:02 +0900

    <작업일시> 24-10-24 16:22
    <작업자>
    김규현
    
    <작업내용>
    1. elasticsearch, kafka, kibana 관련 설정 및 구현
    2. 도커에서 스프링, elasticsearch, kafka, fibana, websocket 실행되도록 설정
    3. 웹서버 이전에 따른 고정 ip주소 변경
    4. 웹소켓 구현 위한 웹소켓 컨트롤러 생성

--- src/main/java/home/project/service/LogConsumerService.java
+++ src/main/java/home/project/service/LogConsumerService.java
@@ -1,6 +1,8 @@
 package home.project.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -8,18 +10,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class LogConsumerService {
+
+    private final SimpMessagingTemplate messagingTemplate;  // WebSocket 메시지 전송용
+
 
     @KafkaListener(topics = "product-view-log", groupId = "log-consumers")
     public void consumeProductViewLog(String message) {
         System.out.println("Consumed product view log: " + message);
         sendLogToElk("product-view-log", message);
+        sendLogToWebSocket(message);  // WebSocket으로 전송
+
     }
 
     @KafkaListener(topics = "purchase-activity-log", groupId = "log-consumers")
     public void consumePurchaseLog(String message) {
         System.out.println("Consumed purchase log: " + message);
         sendLogToElk("purchase-activity-log", message);
+        sendLogToWebSocket(message);  // WebSocket으로 전송
+    }
+
+    // WebSocket을 통해 클라이언트에게 로그 전송
+    private void sendLogToWebSocket(String message) {
+        messagingTemplate.convertAndSend("/topic/logs", message);
     }
 
     // ELK로 로그 전송 로직 (HTTP 방식)
