1a98202e5e7e471ca4769de8a15ee964eef71515
kimpokpung <gh8897@naver.com>
2024-11-05 19:08:30 +0900

    <작업일시> 24-11-05 18:09
    <작업자>
    김규현
    
    <작업내용>
    1. 키(바로 깃에서 삭제할것)

---
+++ src/main/generated/home/project/domain/QOrders.java
@@ -0,0 +1,66 @@
+package home.project.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QOrders is a Querydsl query type for Orders
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QOrders extends EntityPathBase<Orders> {
+
+    private static final long serialVersionUID = 1565891979L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QOrders orders = new QOrders("orders");
+
+    public final NumberPath<Long> amount = createNumber("amount", Long.class);
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final QMember member;
+
+    public final DateTimePath<java.time.LocalDateTime> orderDate = createDateTime("orderDate", java.time.LocalDateTime.class);
+
+    public final StringPath orderNum = createString("orderNum");
+
+    public final NumberPath<Long> pointsEarned = createNumber("pointsEarned", Long.class);
+
+    public final NumberPath<Long> pointsUsed = createNumber("pointsUsed", Long.class);
+
+    public final ListPath<ProductOrder, QProductOrder> productOrders = this.<ProductOrder, QProductOrder>createList("productOrders", ProductOrder.class, QProductOrder.class, PathInits.DIRECT2);
+
+    public final QShipping shipping;
+
+    public QOrders(String variable) {
+        this(Orders.class, forVariable(variable), INITS);
+    }
+
+    public QOrders(Path<? extends Orders> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QOrders(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QOrders(PathMetadata metadata, PathInits inits) {
+        this(Orders.class, metadata, inits);
+    }
+
+    public QOrders(Class<? extends Orders> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.member = inits.isInitialized("member") ? new QMember(forProperty("member")) : null;
+        this.shipping = inits.isInitialized("shipping") ? new QShipping(forProperty("shipping"), inits.get("shipping")) : null;
+    }
+
+}
+
