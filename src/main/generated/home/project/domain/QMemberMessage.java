74e291fe2bdabd84d4fafcdd956ef7d274f406c3
kms199406 <sksdmltkek14@gmail.com>
2024-11-07 01:58:22 +0900

    <작업일시> 202411070150 <작업자> 강민서
    <작업내용>
    수정사항 수정

---
+++ src/main/generated/home/project/domain/QMemberMessage.java
@@ -0,0 +1,53 @@
+package home.project.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QMemberMessage is a Querydsl query type for MemberMessage
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QMemberMessage extends EntityPathBase<MemberMessage> {
+
+    private static final long serialVersionUID = 868117159L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QMemberMessage memberMessage = new QMemberMessage("memberMessage");
+
+    public final StringPath content = createString("content");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final QMember member;
+
+    public QMemberMessage(String variable) {
+        this(MemberMessage.class, forVariable(variable), INITS);
+    }
+
+    public QMemberMessage(Path<? extends MemberMessage> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QMemberMessage(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QMemberMessage(PathMetadata metadata, PathInits inits) {
+        this(MemberMessage.class, metadata, inits);
+    }
+
+    public QMemberMessage(Class<? extends MemberMessage> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.member = inits.isInitialized("member") ? new QMember(forProperty("member")) : null;
+    }
+
+}
+
