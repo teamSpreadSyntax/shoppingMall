cab1f6391dac279b69537985d69ffbec918cca1e
kimkyuhyeon <gh8897@naver.com>
2024-11-16 20:35:52 +0900

    <작성일시>2411162035
    <작성자>
    김규현
    
    <작성내용>
    웹소켓 연결 설정

--- src/main/java/home/project/service/notification/WebSocketNotificationService.java
+++ src/main/java/home/project/service/notification/WebSocketNotificationService.java
@@ -1,12 +1,8 @@
 package home.project.service.notification;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
-
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -17,10 +13,5 @@ public class WebSocketNotificationService {
     public void sendNotification(String destination, String message) {
         messagingTemplate.convertAndSend(destination, message);
         System.out.println("Sent WebSocket notification to " + destination + ": " + message);
-    }
-
-    @MessageMapping("/chat/send")
-    public void sendMsg(@Payload Map<String,Object> data){
-        messagingTemplate.convertAndSend("/topic/1",data);
     }
 }
