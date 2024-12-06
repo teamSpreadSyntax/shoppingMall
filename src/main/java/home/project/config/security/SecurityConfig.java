8767f43e86af1eb82a869242ae92e6302b0ab0aa
kimpokpung <gh8897@naver.com>
2024-12-07 01:43:56 +0900

    <작업일시> 24-12-07 01:43
    <작업자>
    김규현
    
    <작업내용>
    1. 키바나 임베딩 관련 CSP오류 해결 위한 수정

--- src/main/java/home/project/config/security/SecurityConfig.java
+++ src/main/java/home/project/config/security/SecurityConfig.java
@@ -116,7 +116,7 @@ public class SecurityConfig {
                                 .policyDirectives(
                                         "default-src 'self';" +
                                                 "script-src 'self' 'sha256-SbBSU7MfZFnVMq4PuE/jbBz7pPIfXUTYDrdHl7Ckchc=' https://*.projectkkk.com https://projectkkk.vercel.app https://www.projectkkk.com;" +
-                                                "style-src 'self' 'unsafe-inline' https://*.projectkkk.com https://projectkkk.vercel.app;" +
+                                                "style-src 'self' 'sha256-7Qf8U/WgiKRW4EKHHq52AbsaG2K/o6jKbzN5oiiJSXM=' https://*.projectkkk.com https://projectkkk.vercel.app;" +
                                                 "img-src 'self' data: blob:;" +
                                                 "connect-src 'self' https://*.projectkkk.com wss://*.projectkkk.com https://projectkkk.vercel.app;" +
                                                 "font-src 'self' data:;" +
