aae4254e9ab456d346f55bc8b90d3bf292936251
kms199406 <sksdmltkek14@gmail.com>
2024-12-11 17:08:27 +0900

    <작업일시> 202412091827 <작업자> 강민서
    <작업내용>
    테스트

--- src/main/java/home/project/dto/responseDTO/NotificationDetailResponse.java
+++ src/main/java/home/project/dto/responseDTO/NotificationDetailResponse.java
@@ -1,6 +1,7 @@
 package home.project.dto.responseDTO;
 
 import home.project.domain.notification.NotificationType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,9 +9,16 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Schema(description = "알림 세부 응답")
 public class NotificationDetailResponse {
+
+    @Schema(description = "알림 유형", example = "ORDER_COMPLETE")
     private NotificationType notificationType;
+
+    @Schema(description = "알림 설명", example = "주문이 완료되었습니다.")
     private String description;
+
+    @Schema(description = "생성일")
     private LocalDateTime createdAt;
 
     public NotificationDetailResponse(NotificationType notificationType, String description, LocalDateTime createdAt) {
--- src/main/java/home/project/dto/responseDTO/WishListResponse.java
+++ src/main/java/home/project/dto/responseDTO/WishListResponse.java
@@ -1,14 +1,24 @@
 package home.project.dto.responseDTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Schema(description = "위시리스트 응답")
 public class WishListResponse {
+
+    @Schema(description = "위시리스트 ID", example = "1")
     private Long id;
+
+    @Schema(description = "상품 ID", example = "1001")
     private Long productId;
+
+    @Schema(description = "좋아요 여부", example = "true")
     private boolean liked;
+
+    @Schema(description = "메시지", example = "위시리스트에 추가되었습니다.")
     private String message;
 
     public WishListResponse(Long id, Long productId, boolean liked, String message) {
