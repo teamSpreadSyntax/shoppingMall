1b67ecf231cb126e36746efec8933bdec6ce934b
kimpokpung <gh8897@naver.com>
2024-12-12 16:25:59 +0900

    <작업일시> 24-12-12 13:00
    <작업자>
    김규현
    
    <작업내용>
    1. 리뷰 생성 로직 구매내역 있는 상품에 한해 작성 가능하도록 수정

---
+++ src/main/java/home/project/repository/product/ProductVariantRepository.java
@@ -0,0 +1,10 @@
+//package home.project.repository.product;
+//
+//
+//import home.project.domain.product.ProductVariant;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface ProductVariantRepository extends JpaRepository<ProductVariant, Long> {
+//    boolean existsByProductNum(String productNum);
+//
+//}
