367b7541bb1faef2d36cfd0211568d8896529d2a
강민서 <138178615+kms199406@users.noreply.github.com>
2024-12-30 00:48:04 +0900

    202412232354 <작업자> 강민서 <작업내용> 이미지저장

---
+++ src/main/java/home/project/config/GoogleCloudStorageConfig.java
@@ -0,0 +1,15 @@
+package home.project.config;
+
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GoogleCloudStorageConfig {
+
+    @Bean
+    public Storage storage() {
+        return StorageOptions.getDefaultInstance().getService();
+    }
+}
